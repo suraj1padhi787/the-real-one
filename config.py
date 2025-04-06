c313f2a4c94402f292b5b22c9f041036f5e74089
suraj1padhi787 <suraj1padhi@gmail.com>
2025-04-06 21:10:24 +0530

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = "5746709"
 API_HASH = "c00191beb5cdedc2bacf532977d07cd8"
-BOT_TOKEN = "7906653700:AAFQIAyXETAYrpTxPL7cwuddHEe8RZdOG3Y"
+BOT_TOKEN = "8007463725:AAFUuqGNBGNxM_NI-I2JM6V59OySYlfabdo"
 DB_PATH = "sessions.db"
 ADMIN_ID = 5893249491  # ← yahi tumhara Telegram user ID hona chahiye (as admin)
 
